1ce27b303eeebe4ec977d4d2cedef659e86f4e8d
Anthony Librizzi <librizzia@gmail.com>
2022-02-15 00:11:01 -0500

    fix build error due to misnamed variables

--- src/App.js
+++ src/App.js
@@ -12,47 +12,59 @@ import { UNCATEGORIZED_BUDGET_ID, useBudgets } from "./contexts/BudgetsContext"
 function App() {
   const [showAddBudgetModal, setShowAddBudgetModal] = useState(false)
   const [showAddExpenseModal, setShowAddExpenseModal] = useState(false)
-  const [showViewExpensesModalBudgetId, setViewExpensesModalBudgetId] = useState()
-  const [showAddExpenseModalId, setAddExpenseModalId] = useState()
+  const [viewExpensesModalBudgetId, setViewExpensesModalBudgetId] = useState()
+  const [addExpenseModalBudgetId, setAddExpenseModalBudgetId] = useState()
   const { budgets, getBudgetExpenses } = useBudgets()
 
   function openAddExpenseModal(budgetId) {
     setShowAddExpenseModal(true)
-    setAddExpenseModalId(budgetId)
+    setAddExpenseModalBudgetId(budgetId)
   }
-
 
   return (
     <>
       <Container className="my-4">
         <Stack direction="horizontal" gap="2" className="mb-4">
-          <h1 className="me-auto">React Budget App</h1>
-          <Button variant="primary" onClick={() => setShowAddBudgetModal(true)}>Add Budget</Button>
-          <Button variant="outline-primary" onClick={openAddExpenseModal}>Add Expense</Button>
+          <h1 className="me-auto">Budgets</h1>
+          <Button variant="primary" onClick={() => setShowAddBudgetModal(true)}>
+            Add Budget
+          </Button>
+          <Button variant="outline-primary" onClick={openAddExpenseModal}>
+            Add Expense
+          </Button>
         </Stack>
         <div
           style={{
             display: "grid",
-            gridTemplateColumns: "repeat(autofill, minmax(300px, 1fr))",
+            gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
             gap: "1rem",
-            alignItems: "flex-start"
-          }}>
-
+            alignItems: "flex-start",
+          }}
+        >
           {budgets.map(budget => {
-            const amount = getBudgetExpenses(budget.id).reduce((total, expense) => total + expense.amount, 0)
+            const amount = getBudgetExpenses(budget.id).reduce(
+              (total, expense) => total + expense.amount,
+              0
+            )
             return (
-              < BudgetCard
+              <BudgetCard
                 key={budget.id}
                 name={budget.name}
                 amount={amount}
                 max={budget.max}
                 onAddExpenseClick={() => openAddExpenseModal(budget.id)}
-                onViewExpensesClick={() => setViewExpensesModalBudgetId(budget.id)}
+                onViewExpensesClick={() =>
+                  setViewExpensesModalBudgetId(budget.id)
+                }
               />
             )
           })}
-          <UncategorizedBudgetCard onAddExpenseClick={openAddExpenseModal}
-            onViewExpensesClick={() => setViewExpensesModalBudgetId(UNCATEGORIZED_BUDGET_ID)} />
+          <UncategorizedBudgetCard
+            onAddExpenseClick={openAddExpenseModal}
+            onViewExpensesClick={() =>
+              setViewExpensesModalBudgetId(UNCATEGORIZED_BUDGET_ID)
+            }
+          />
           <TotalBudgetCard />
         </div>
       </Container>
@@ -62,11 +74,11 @@ function App() {
       />
       <AddExpenseModal
         show={showAddExpenseModal}
-        defaultBudgetId={setAddExpenseModalId}
+        defaultBudgetId={addExpenseModalBudgetId}
         handleClose={() => setShowAddExpenseModal(false)}
       />
       <ViewExpensesModal
-        budgetId={showViewExpensesModalBudgetId}
+        budgetId={viewExpensesModalBudgetId}
         handleClose={() => setViewExpensesModalBudgetId()}
       />
     </>
